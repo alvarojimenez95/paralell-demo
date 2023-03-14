1fa3e279155285397eefad9d1d0030e32d3d92c5
alvarojimenez95 <alvjimenez1995@gmail.com>
2023-03-14 11:53:20 +0100

    first commit

--- spotify/client.py
+++ spotify/client.py
@@ -4,12 +4,15 @@ from base64 import b64encode, b64decode
 from pprint import pprint
 from urllib.parse import urlparse, parse_qs
 import webbrowser
+
 class HTTPClient:
     token_url = "https://accounts.spotify.com/api/token"
+    base_url = "https://api.spotify.com/v1"
     def __init__(self, client_id: str, client_secret: str):
         self.client_id = client_id
         self.client_secret = client_secret
         self.token = None
+        self._session = requests.Session()
 
     def _pepare_header(self):
         encoded_credentials = b64encode(f"{self.client_id}:{self.client_secret}".encode('utf-8')).decode("ascii")
@@ -20,7 +23,23 @@ class HTTPClient:
         resp = requests.post(self.token_url, headers = headers, params = {"grant_type" : "client_credentials"})
         resp.raise_for_status()
         self.token = resp.json()["access_token"]
+
+    def call(self, method, endpoint, params = None, body = None, headers = None):
+        url = self.base_url + endpoint
+        if headers:
+            headers.update(self._prepare_auth_header())
+        else:
+            headers = self._prepare_auth_header()
+        with self._session as session:
+            resp = session.request(method = method, url = url, params = params, headers = headers, json = body)
+            resp.raise_for_status()
+            return resp.json()
+        
+    def get(self, endpoint, params = None, headers = None):
+        return self.call("GET", endpoint, params, headers = headers)
     
+    def _prepare_auth_header(self):
+        return {"Authorization" : f"Bearer {self.token}"}
 
         
 
--- spotify/spotify_client.py
+++ spotify/spotify_client.py
@@ -6,28 +6,19 @@ class Spotify:
         self.client = HTTPClient(client_id=client_id, client_secret=client_secret)
         self.client.retrieve_token()
 
-    def _prepare_header(self):
-        return {"Authorization" : f"Bearer {self.client.token}"}
-
     def get_track(self, track_id: str):
-        headers = self._prepare_header()
-        url = f"https://api.spotify.com/v1/tracks/{track_id}"
-        resp = requests.get(url = url, headers=headers)
-        return resp.json() 
+        endpoint = f"/tracks/{track_id}"
+        resp = self.client.get(endpoint = endpoint)
+        return resp
     
     def get_artist(self, artist_id: str):
-        headers = self._prepare_header()
-        url = f"https://api.spotify.com/v1/artists/{artist_id}"
-        resp = requests.get(url = url, headers = headers)
-        return resp.json()
+        endpoint = f"/artists/{artist_id}"
+        resp = self.client.get(endpoint = endpoint)
+        return resp
 
 
     def get_artist_track_ids(self, artist_name: str = None, number: int = 100):
-        headers = self._prepare_header()
-        url = f"https://api.spotify.com/v1/search"
-        offset = 0
-        # track_ids = []
-        # while offset < number:
+        endpoint = "/search"
         params = {"limit" : 20, "q" : artist_name, "type": "track"}
-        resp = requests.get(url = url, params = params, headers = headers)
-        return resp.json()
+        resp = self.client.get(endpoint = endpoint, params = params)
+        return resp
