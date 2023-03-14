0946ab4a22d00f606a978e141e0728850a40941c
alvarojimenez95 <alvjimenez1995@gmail.com>
2023-03-14 11:39:42 +0100

    first commit

---
+++ spotify/client.py
@@ -0,0 +1,28 @@
+import requests
+from typing import Optional, List, Mapping
+from base64 import b64encode, b64decode
+from pprint import pprint
+from urllib.parse import urlparse, parse_qs
+import webbrowser
+class HTTPClient:
+    token_url = "https://accounts.spotify.com/api/token"
+    def __init__(self, client_id: str, client_secret: str):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.token = None
+
+    def _pepare_header(self):
+        encoded_credentials = b64encode(f"{self.client_id}:{self.client_secret}".encode('utf-8')).decode("ascii")
+        return {"Authorization" : f"Basic {encoded_credentials}", "Content-Type" : "application/x-www-form-urlencoded"}
+    
+    def retrieve_token(self):
+        headers = self._pepare_header()
+        resp = requests.post(self.token_url, headers = headers, params = {"grant_type" : "client_credentials"})
+        resp.raise_for_status()
+        self.token = resp.json()["access_token"]
+    
+
+        
+
+
+    
